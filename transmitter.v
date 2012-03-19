68e5be8b17b7ee834de7063e330fc54bcbe58284
C.c <ccik@inbox.ru>
2012-03-19 20:16:13 +0400

    fuck the git

---
+++ transmitter.v
@@ -0,0 +1,25 @@
+timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:49:33 03/19/2012 
+// Design Name: 
+// Module Name:    transmitter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module transmitter(
+    );
+
+
+endmodule
